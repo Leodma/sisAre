c5206a7be74bb1d55de5edbf5ef4ee92b587ec53
Leo <leodma@gmail.com>
2019-08-08 16:05:14 -0300

    esqueci da porra dos :

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
     try:
-        if not  User.query.filter_by(user_name='leo').first()
+        if not  User.query.filter_by(user_name='leo').first():
             User.create_user(user='Leo', email = 'leodma@gmail.com', nivel='Adm', password = 'leo123')
     except exc.IntegrityError:
         flask_app.run()
