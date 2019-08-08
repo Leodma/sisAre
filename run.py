e6ccb30987daa8847f02ec016829921d1a07b8af
Leo <leodma@gmail.com>
2019-08-08 10:52:02 -0300

    Adicionando tratamento de integridade no run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from app import create_app,db
 from app.auth.models import User
+from sqlalchemy import exc
+
 
 # remova a linha abaixo para rodar localmente
 # if __name__ == "__main__":
@@ -7,8 +9,8 @@ flask_app = create_app('prod')
     
 with flask_app.app_context():
     db.create_all()
-
-
-
-
-flask_app.run()
+    try:
+        if not User.query.filter_by(user_email="teste").first():
+            User.create_user(user='teste', email = teste@teste.com.br, nivel = 'Conv', password =  't&st&sisare')
+    except exc.IntegrityError():
+        flask_app.run()
