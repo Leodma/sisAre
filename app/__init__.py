8c7fa5f997bb3de9325f55019afa802dfeb3169c
Leo <leodma@gmail.com>
2019-08-08 18:21:35 -0300

    retirando o favico do app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
 
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -22,8 +22,7 @@ def create_app(config_type): # dev, test ou prod
     configuration = os.path.join(os.getcwd(),'config', config_type + '.py')
     app.config.from_pyfile(configuration)
     app.register_error_handler(404, page_not_found)
-    app.add_url_rule('/favicon.ico',redirect_to=url_for('static', filename='SisAre.ico'))
-
+    
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
