4c6a2e93f9ef15c9a01dc765f4925ebc0938b168
Leo <leodma@gmail.com>
2019-08-08 12:36:03 -0300

    run py again

--- run.py
+++ run.py
@@ -9,8 +9,5 @@ flask_app = create_app('prod')
     
 with flask_app.app_context():
     db.create_all()
-    try:
-        if not User.query.filter_by(user_email="teste").first():
-            User.create_user(user='teste', email = 'teste@teste.com.br', nivel = 'Conv', password =  't&st&sisare')
-    except exc.IntegrityError():
-        flask_app.run()
+ 
+    flask_app.run()
